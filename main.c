2908db5d07d48ff834b13efb5706fa233595cdf8
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-04-10 07:04:02 +0530

    added print_notes function to print the number of notes in each denominations

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
 #include "atm.h"
 
-int main(void)
-{
-  unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
-  for (int i = 0; i < 6; i++)
-  {
-    unsigned int notes = get_money(amounts[i]);
-    printf("%5u %08x\n", amounts[i], notes);
-  }
+void print_notes(notes notes) {
+  money denominations[] = DENOMINATIONS;
 
+  REPEAT_8_TIMES {
+    printf("%d notes of Rs %d\n", notes >> 28, denominations[index]);
+    notes <<= 4;
+  }
+}
+
+int main(void) {
+  money amouts[] = {1, 10, 39, 589, 2590, 31999};
+  for (int index = 0; index < 6; index++) {
+    notes notes = get_money(amouts[index]);
+    printf("\nAmount: Rs. %d\n", amouts[index]);
+    print_notes(notes);
+    printf("\n");
+  }
   return 0;
 }
